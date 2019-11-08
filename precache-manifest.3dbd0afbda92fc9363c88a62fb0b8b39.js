cbf1042a07c76cbef6f82e9b7738f9b3dbf54ec2
VadymYakovets <vad.yakovets@gmail.com>
2019-11-08 22:15:32 +0200

    Updates

---
+++ precache-manifest.3dbd0afbda92fc9363c88a62fb0b8b39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c684163e1c9db0cf3bc22e4967d2f783",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "4ae7bc4c90faaf360602",
+    "url": "/goit-react-hw-01-components/static/css/main.8c5dcaa1.chunk.css"
+  },
+  {
+    "revision": "c31b19ee70630d468928",
+    "url": "/goit-react-hw-01-components/static/js/2.617d7456.chunk.js"
+  },
+  {
+    "revision": "4ae7bc4c90faaf360602",
+    "url": "/goit-react-hw-01-components/static/js/main.a0313175.chunk.js"
+  },
+  {
+    "revision": "b41daedff6819c621acb",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.ac6bac53.js"
+  },
+  {
+    "revision": "f9203469f750b4284c62aa49f9496cb0",
+    "url": "/goit-react-hw-01-components/static/media/lato-v15-latin-700.f9203469.eot"
+  },
+  {
+    "revision": "056a373d808ada4f12a2d6538e88823c",
+    "url": "/goit-react-hw-01-components/static/media/lato-v15-latin-regular.056a373d.eot"
+  }
+]);
